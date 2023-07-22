fb49a813f69807e9edf99de71583c9d28aa9c4ef
ldavi255 <134437533+ldavi255@users.noreply.github.com>
2023-07-22 19:41:58 -0400

    Add files via upload

---
+++ achievement.test.py
@@ -0,0 +1,55 @@
+def display_achievements(games):
+    print("Halo: The Master Chief Collection - Achievements Unlocked:")
+    for game, achievements in games.items():
+        print(f"{game}:")
+        for achievement in achievements:
+            print(f" - {achievement}")
+        print()
+
+def add_achievement(games, game, achievement):
+    if game in games:
+        games[game].append(achievement)
+    else:
+        games[game] = [achievement]
+    print(f"Achievement '{achievement}' added to '{game}'.")
+
+def remove_achievement(games, game, achievement):
+    if game in games and achievement in games[game]:
+        games[game].remove(achievement)
+        print(f"Achievement '{achievement}' removed from '{game}'.")
+    else:
+        print("Achievement not found.")
+
+def main():
+    halo_achievements = {
+        "Halo: Combat Evolved": ["Pillar of Autumn", "Halo", "Truth and Reconciliation"],
+        "Halo 2": ["The Heretic", "Cairo Station", "Outskirts"],
+        "Halo 3": ["Sierra 117", "Crow's Nest", "Tsavo Highway"],
+        # Add more Halo games and achievements here
+    }
+
+    while True:
+        print("1. View Achievements")
+        print("2. Add Achievement")
+        print("3. Remove Achievement")
+        print("4. Quit")
+
+        choice = int(input("Enter your choice (1/2/3/4): "))
+
+        if choice == 1:
+            display_achievements(halo_achievements)
+        elif choice == 2:
+            game = input("Enter the Halo game name: ")
+            achievement = input("Enter the achievement unlocked: ")
+            add_achievement(halo_achievements, game, achievement)
+        elif choice == 3:
+            game = input("Enter the Halo game name: ")
+            achievement = input("Enter the achievement to remove: ")
+            remove_achievement(halo_achievements, game, achievement)
+        elif choice == 4:
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
